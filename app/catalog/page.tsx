fc4035530010f45a22a55e7be57cece7a7232df7
Aleksei <bylexatime@gmail.com>
2025-12-07 17:51:17 +0200

    fix metadata

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,6 +1,20 @@
 import CamperFilters from '@/components/CamperFilters/CamperFilters';
 import CamperList from '@/components/CamperList/CamperList';
 import css from './Catalog.module.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Catalog | TravelTrucks',
+  description:
+    'Rent the best campers in Ukraine. Fully equipped, comfortable and ready for adventure.',
+
+  openGraph: {
+    title: '🚐 Rent Your Dream Camper | TravelTrucks',
+    description:
+      'Find the perfect camper for your trip. AC, Kitchen, Shower & more!',
+    type: 'website',
+  },
+};
 
 export default function CatalogPage() {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import Hero from '@/components/Hero/Hero';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'TravelTrucks',
+  description:
+    'Rent the best campers in Ukraine. Fully equipped, comfortable and ready for adventure.',
+  openGraph: {
+    title: 'TravelTrucks',
+    description:
+      'Find the perfect camper for your trip. AC, Kitchen, Shower & more!',
+    type: 'website',
+  },
+};
 export default function Home() {
   return (
     <main>
